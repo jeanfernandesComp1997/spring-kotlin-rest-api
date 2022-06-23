5a5a7abb1fce2edb5531bdd154d4136db740b56e
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-23 20:23:43 -0300

    Upload Project

---
+++ src/main/kotlin/br/com/localdemo/forum/HelloController.kt
@@ -0,0 +1,16 @@
+package br.com.localdemo.forum
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("hello")
+class HelloController {
+
+    @GetMapping
+    fun hello(): ResponseEntity<String> {
+        return ResponseEntity.ok("Hello World")
+    }
+}
