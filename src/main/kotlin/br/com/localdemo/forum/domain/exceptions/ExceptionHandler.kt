8ef65464f94487ac0980691e6998630ccca89671
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-30 17:56:55 -0300

    If applied, add validation bad request exception handler

--- src/main/kotlin/br/com/localdemo/forum/domain/exceptions/ExceptionHandler.kt
+++ src/main/kotlin/br/com/localdemo/forum/domain/exceptions/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package br.com.localdemo.forum.domain.exceptions
 
 import br.com.localdemo.forum.domain.dto.ExceptionView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import javax.servlet.http.HttpServletRequest
@@ -33,6 +34,26 @@ class ExceptionHandler {
             status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             error = HttpStatus.INTERNAL_SERVER_ERROR.name,
             message = exception.message,
+            path = request.servletPath
+        )
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ExceptionView {
+        val errorMessage = HashMap<String, String?>()
+
+        exception.bindingResult.fieldErrors.forEach { e ->
+            errorMessage[e.field] = e.defaultMessage
+        }
+
+        return ExceptionView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
             path = request.servletPath
         )
     }
