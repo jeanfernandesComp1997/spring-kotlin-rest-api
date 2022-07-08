dfec9b9bc7f87a22573a8af6570490efd721de48
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-07-07 22:22:26 -0300

    Add JWTFilter

--- src/main/kotlin/br/com/localdemo/forum/application/securiry/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/localdemo/forum/application/securiry/SecurityConfiguration.kt
@@ -1,23 +1,41 @@
 package br.com.localdemo.forum.application.securiry
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration() {
+class SecurityConfiguration(
+    private val configuration: AuthenticationConfiguration,
+    private val jwtUtil: JwtUtil
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.authorizeHttpRequests().antMatchers("/topics").hasAuthority("READ_WRITE").anyRequest().authenticated()
-            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().formLogin().disable().httpBasic()
+        http.authorizeHttpRequests()
+            .antMatchers("/topics").hasAuthority("READ_WRITE")
+            .antMatchers("/login").permitAll().anyRequest().authenticated()
+
+        http.addFilterBefore(
+            JWTLoginFilter(authManager = configuration.authenticationManager, jwtUtil = jwtUtil),
+            UsernamePasswordAuthenticationFilter().javaClass
+        )
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().formLogin().disable()
+            .httpBasic()
 
         return http.build()
     }
---
+++ src/main/kotlin/br/com/localdemo/forum/domain/entities/Credentials.kt
@@ -0,0 +1,6 @@
+package br.com.localdemo.forum.domain.entities
+
+data class Credentials(
+    val username: String,
+    val password: String
+)
