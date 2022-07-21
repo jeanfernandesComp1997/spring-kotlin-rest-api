8732d3d29fe2e028df4f658f6045b4ca15b20cd8
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-07-20 22:29:37 -0300

    Parcial security error handling

--- src/main/kotlin/br/com/localdemo/forum/application/security/JWTAuthenticationFilter.kt
+++ src/main/kotlin/br/com/localdemo/forum/application/security/JWTAuthenticationFilter.kt
@@ -1,27 +1,38 @@
 package br.com.localdemo.forum.application.security
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
+import org.springframework.web.servlet.HandlerExceptionResolver
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-class JWTAuthenticationFilter(private val jwtUtil: JwtUtil) : OncePerRequestFilter() {
+
+class JWTAuthenticationFilter(
+    private val jwtUtil: JwtUtil,
+    private var resolver: HandlerExceptionResolver
+) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        val token = request.getHeader("Authorization")
-        val jwt = getTokenDetail(token)
+        try {
+            val token = request.getHeader("Authorization")
+            val jwt = getTokenDetail(token)
 
-        if (jwtUtil.isValid(jwt)) {
-            val authentication = jwtUtil.getAuthentication(jwt)
-            SecurityContextHolder.getContext().authentication = authentication
+            if (jwtUtil.isValid(jwt)) {
+                val authentication = jwtUtil.getAuthentication(jwt)
+                SecurityContextHolder.getContext().authentication = authentication
+            }
+
+            filterChain.doFilter(request, response)
+        } catch (e: Exception) {
+            resolver.resolveException(request, response, null, e);
         }
-
-        filterChain.doFilter(request, response)
     }
 
     private fun getTokenDetail(token: String?): String? {
--- src/main/kotlin/br/com/localdemo/forum/application/security/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/localdemo/forum/application/security/SecurityConfiguration.kt
@@ -1,28 +1,27 @@
 package br.com.localdemo.forum.application.security
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-
+import org.springframework.web.servlet.HandlerExceptionResolver
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 class SecurityConfiguration(
     private val configuration: AuthenticationConfiguration,
     private val jwtUtil: JwtUtil,
-    private val authEntryPoint: DelegatedAuthenticationEntryPoint
+    private val authEntryPoint: DelegatedAuthenticationEntryPoint,
+    @Qualifier("handlerExceptionResolver")
+    private var resolver: HandlerExceptionResolver
 ) {
 
     @Bean
@@ -44,7 +43,7 @@ class SecurityConfiguration(
             UsernamePasswordAuthenticationFilter().javaClass
         )
 
-        http.addFilterBefore(JWTAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http.addFilterBefore(JWTAuthenticationFilter(jwtUtil, resolver), UsernamePasswordAuthenticationFilter().javaClass)
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
