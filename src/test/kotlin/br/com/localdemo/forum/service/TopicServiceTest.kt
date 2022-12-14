6304f90f2dfc59264338da74acd6245c20053aac
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-12-13 22:08:08 -0300

    test: Add default test

---
+++ src/test/kotlin/br/com/localdemo/forum/service/TopicServiceTest.kt
@@ -0,0 +1,15 @@
+package br.com.localdemo.forum.service
+
+import org.junit.jupiter.api.Test
+
+class TopicServiceTest {
+
+    @Test
+    fun `Should return sucess when call get topic by id`() {
+        // Act
+        val result = true
+
+        // Assert
+        assert(result)
+    }
+}
